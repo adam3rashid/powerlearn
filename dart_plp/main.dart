2c4f8737d7554c58c945880320141dfdf9120b69
Adam Rashid <rashidadamjee879@gmail.com>
2024-02-27 12:57:04 +0300

    Initial Commit

---
+++ dart_plp/main.dart
@@ -0,0 +1,7 @@
+void main() {
+  // single-line comments
+  /* 
+        Multi-line comments
+    */
+  print("Hello World! This is my first dart code");
+}
---
+++ dart_plp/variables.dart
@@ -0,0 +1,22 @@
+void main() {
+  // declare variables
+  String name = "Dart Class";
+  int age = 66;
+  double price = 100.98;
+  var year = 2024;
+  num students = 460;
+  bool isGreat = true;
+
+  print(name);
+  print(age);
+  print(price);
+  print(year);
+  print(students);
+
+  print("Welcome to $name.");
+  print("It has run for $age years.");
+  print("The price of this class is Ksh. $price");
+  print("Register early for the $year cohosrt.");
+  print("We are currently at $students students.");
+  print("Many say that is is a great program, $isGreat");
+}
